91a2742bc81777fa31ef84efe345fbf924073a9f
Educrepaldi <97613407+Educrepaldi@users.noreply.github.com>
2022-01-12 17:16:20 +0100

    Delete HolaMundo directory

--- HolaMundo/src/com/company/Main.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("HOLA MUNDO");
-        System.out.println("EDICION PRUEBECIÑA NUMERO 1");
-        System.out.println("cambios 2");}
-}
